5d7a535d3dd725b958da48039403b59e3c763023
CartoonsFun <roman.iovlev.jdi@gmail.com>
2022-09-24 12:43:31 +0200

    add from username

--- src/events/feedback-event.ts
+++ src/events/feedback-event.ts
@@ -5,7 +5,7 @@ export class FeedbackEvent {
 
     async process() {
         const text = this.feedback.textTemplate
-            ? this.feedback.textTemplate.replace('{{text}}', this.ctx.text)
+            ? this.feedback.textTemplate.replace('{{text}}', this.ctx.text).replace('{{from}}', this.ctx.from.username)
             : this.ctx.text;
         await this.bot.sendMessage(this.feedback.chatId, text, {parse_mode: 'Markdown'});
     }
